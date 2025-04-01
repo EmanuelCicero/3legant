6c1fc559d9a39fe3210be7bc0681ad6e12b487bc
lucassec1 <lucasemmanuelcs@gmail.com>
2025-03-31 21:43:30 -0300

    :sparkles: feat: add Uuid value object and related types #12

---
+++ server/src/core/shared/domain/value-objects/uuid.vo.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+import { ValueObject } from './value-object';
+
+export class Uuid extends ValueObject {
+  public readonly id: string;
+
+  public static mapFrom(uuids: Uuid[]): Map<string, Uuid> {
+    return new Map<string, Uuid>(uuids.map((uuid) => [uuid.id, uuid]));
+  }
+
+  constructor(id: string) {
+    super();
+    this.id = id || uuidv4();
+    this.validate();
+  }
+
+  private validate(): void {
+    const isValid = uuidValidate(this.id);
+    if (!isValid) {
+      throw new Error('ID must be a valida UUID');
+    }
+  }
+
+  public toString(): string {
+    return this.id;
+  }
+}
