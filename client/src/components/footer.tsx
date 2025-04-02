b503d1ae9cce202da83597ac111e5b5d96a6541a
EmanuelCicero <emanuelcicero40@gmail.com>
2025-04-02 10:21:49 -0300

    :recycle: refactor: enhance footer component structure and add legal and social links

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -5,52 +5,63 @@ import YouTubeIcon from '../assets/youtube-icon.svg';
 
 export default function footer(){
   const NavLinks = () => (
-    <ul className="flex md:flex-row gap-10 text-neutral-300 text-sm text-center flex-col">
+    <ul className="flex md:flex-row gap-10 text-neutral-300 text-sm text-center flex-col py-10">
       {['Home', 'Shop', 'Product', 'Blog', 'Contact Us'].map((item) => (
         <li key={item}><a href="" className='hover:text-white'>{item}</a></li>
       ))}
     </ul>
   );
 
+  const LegalLinks = () => (
+    <ul className="flex flex-row gap-7 flex-wrap justify-center text-center">
+      {['Privacy Policy', 'Terms of Use'].map((item) => (
+        <li key={item}><a href="" className='text-neutral-100 font-semibold'>{item}</a></li>
+      ))}
+    </ul>
+  );
+
+  const SocialLinks = () => {
+
+    const icons = [
+      {src: InstagramIcon, name: 'Instagram'},
+      {src: FacebookIcon, name: 'Facebook'},
+      {src: YouTubeIcon, name: 'YouTube'}
+    ];
+
+    return(
+      <ul className='flex flex-row gap-6 justify-center'>
+        {icons.map(({ src, name }) => (
+          <li key={name}><a href=""><Image src={src} alt={name} width={24} height={24} /></a></li>
+        ))}
+      </ul>
+    );
+  };
+
   return(
-    <footer className="bg-neutral-700 text-white flex flex-col py-12 px-5">
-      <div className="container mx-auto flex justify-between md:flex-nowrap flex-wrap my-10">
+    <footer className="bg-neutral-700 text-white flex flex-col py-11 px-5">
+      <div className="container mx-auto flex justify-between md:flex-nowrap flex-wrap">
         <div className="flex lg:flex-row flex-col lg:gap-8 gap-4 items-center md:m-0 mx-auto">
           <p className="text-2xl font-medium">3legant<span className="text-neutral-400">.</span></p> 
           <div className="border lg:h-6 lg:w-0 w-6 border-neutral-400"></div>
           <p className="text-neutral-300 text-sm text-center">Gift & Decoration Store</p>
         </div>
-        <nav className="md:w-fit w-full md:items-center md:flex">
+        <nav className="md:w-fit w-full">
           {NavLinks()}
         </nav>
       </div>
       <div className="container hidden md:flex border-t border-neutral-400 mx-auto justify-between py-5">
         <div className="flex flex-row gap-7 flex-wrap pr-3 max-w-[80%]">
           <p className="text-neutral-300">Copyright © 2025 3legant. All rights reserved</p>
-          <div className="flex flex-row gap-7">
-            <a href="" className="text-neutral-100 font-semibold">Privacy Policy</a>
-            <a href="" className="text-neutral-100 font-semibold">Terms of Use</a>
-          </div>
+          {LegalLinks()}
         </div>
-        <div className="flex flex-row gap-6">
-          <a href=""><Image src={InstagramIcon} alt="Instagram" width={24} height={24} /></a>
-          <a href=""><Image src={FacebookIcon} alt="Facebook" width={24} height={24} /></a> 
-          <a href=""><Image src={YouTubeIcon} alt="YouTube" width={24} height={24} /></a>      
-        </div>
+        {SocialLinks()}
       </div>
 
       {/* Mobile */}
       <div className="md:hidden flex flex-col border-t border-neutral-400 mx-auto w-[80%] py-5 flex-wrap gap-8">
-        <div className="flex flex-row gap-6 flex-wrap justify-center">
-          <a href=""><Image src={InstagramIcon} alt="Instagram" width={24} height={24} /></a>
-          <a href=""><Image src={FacebookIcon} alt="Facebook" width={24} height={24} /></a>
-          <a href=""><Image src={YouTubeIcon} alt="YouTube" width={24} height={24} /></a>
-        </div>
+        {SocialLinks()}
         <div className="flex flex-row gap-7 flex-wrap justify-center">
-          <div className="flex flex-row gap-7 flex-wrap text-center justify-center">
-            <a href="" className="text-neutral-100 font-semibold">Privacy Policy</a>
-            <a href="" className="text-neutral-100 font-semibold">Terms of Use</a>
-          </div>
+          {LegalLinks()}
           <p className="text-neutral-300 text-center">Copyright © 2025 3legant. All rights reserved</p>
         </div>
       </div>
